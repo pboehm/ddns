24b7ea605819cd901f9c4a1b65f52444021322b2
Philipp Böhm <philipp@pboehm.org>
2017-11-28 23:54:05 +0100

    Introduce HostLookup and corresponding tests

---
+++ backend/lookup.go
@@ -0,0 +1,93 @@
+package backend
+
+import (
+	"errors"
+	"fmt"
+	"github.com/pboehm/ddns/config"
+	"github.com/pboehm/ddns/hosts"
+	"strings"
+	"time"
+)
+
+type Request struct {
+	QType      string
+	QName      string
+	Remote     string
+	Local      string
+	RealRemote string
+	ZoneId     string
+}
+
+type Response struct {
+	QType   string
+	QName   string
+	Content string
+	TTL     int
+}
+
+type HostLookup struct {
+	config *config.Config
+	hosts  hosts.HostBackend
+}
+
+func (l *HostLookup) Lookup(request *Request) (*Response, error) {
+	responseRecord := request.QType
+	responseContent := ""
+
+	switch request.QType {
+	case "SOA":
+		responseContent = fmt.Sprintf("%s. hostmaster%s. %d 1800 3600 7200 5",
+			l.config.SOAFqdn, l.config.Domain, l.currentSOASerial())
+
+	case "NS":
+		responseContent = l.config.SOAFqdn
+
+	case "A", "AAAA", "ANY":
+		hostname, err := l.extractHostname(request.QName)
+		if err != nil {
+			return nil, err
+		}
+
+		var host *hosts.Host
+		if host, err = l.hosts.GetHost(hostname); err != nil {
+			return nil, err
+		}
+
+		responseContent = host.Ip
+
+		responseRecord = "A"
+		if !host.IsIPv4() {
+			responseRecord = "AAAA"
+		}
+
+		if (request.QType == "A" || request.QType == "AAAA") && request.QType != responseRecord {
+			return nil, errors.New("IP address is not valid for requested record")
+		}
+
+	default:
+		return nil, errors.New("Invalid request")
+	}
+
+	return &Response{QType: responseRecord, QName: request.QName, Content: responseContent, TTL: 5}, nil
+}
+
+// extractHostname extract the host part of the fqdn: pi.d.example.org -> pi
+func (l *HostLookup) extractHostname(rawQueryName string) (string, error) {
+	queryName := strings.ToLower(rawQueryName)
+
+	hostname := ""
+	if strings.HasSuffix(queryName, l.config.Domain) {
+		hostname = queryName[:len(queryName)-len(l.config.Domain)]
+	}
+
+	if hostname == "" {
+		return "", errors.New("Query name does not correspond to our domain")
+	}
+
+	return hostname, nil
+}
+
+// currentSOASerial get the current SOA serial by returning the current time in seconds
+func (l *HostLookup) currentSOASerial() int64 {
+	return time.Now().Unix()
+}
