c2e7b6f2d282bd8b8eadd7b754ab09e936ffcdae
Philipp Böhm <philipp@pboehm.org>
2017-11-28 22:26:59 +0100

    prepare docker setup for http remote backend

--- docker/powerdns/entrypoint.sh
+++ docker/powerdns/entrypoint.sh
@@ -2,8 +2,6 @@
 
 CONFIG_FILE=/etc/powerdns/pdns.conf
 
-sed -i 's/{{DDNS_DOMAIN}}/'"${DDNS_DOMAIN}"'/g' ${CONFIG_FILE}
-sed -i 's/{{DDNS_SOA_DOMAIN}}/'"${DDNS_SOA_DOMAIN}"'/g' ${CONFIG_FILE}
-sed -i 's/{{DDNS_VERBOSE}}/'"${DDNS_VERBOSE}"'/g' ${CONFIG_FILE}
+sed -i 's/{{PDNS_REMOTE_HTTP_HOST}}/'"${PDNS_REMOTE_HTTP_HOST}"'/g' ${CONFIG_FILE}
 
 exec "$@"
